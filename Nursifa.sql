b2bc28121050a7b8b7d1ea472bfd2e6be6c45289
Nursyfa <110141622+nursyfaa@users.noreply.github.com>
2022-08-06 21:29:58 +0700

    belajardb.sql

---
+++ Nursifa.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 06, 2022 at 07:59 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_siswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_siswa`
+--
+
+CREATE TABLE `tbl_siswa` (
+  `id_siswa` varchar(5) NOT NULL,
+  `nama_siswa` varchar(50) DEFAULT NULL,
+  `alamat` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_siswa`
+--
+
+INSERT INTO `tbl_siswa` (`id_siswa`, `nama_siswa`, `alamat`) VALUES
+('1', 'Nursifa', 'Cilaja Buhun'),
+('2', 'Kevin', 'Wanareja'),
+('3', 'Sherly', 'Soklat'),
+('4', 'Dezan', 'Pagaden'),
+('5', 'Yoshi', 'Tambakan');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_siswa`
+--
+ALTER TABLE `tbl_siswa`
+  ADD PRIMARY KEY (`id_siswa`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
